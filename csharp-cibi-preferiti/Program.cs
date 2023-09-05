95331b14a017c02455b5818c577c70435858a45a
StefanoM98 <Utente@PC-STEFANO>
2023-09-05 15:33:05 +0200

    Prova2

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cibiPreferiti = { "Pasta al pesto", "Pasta e patate", "Lasagne", "Spaghetti con le vongole", "Gomiti con uova e ricotta"};
+Console.WriteLine("I miei cibi preferiti sono " +  cibiPreferiti.Length);
+
+for (var i = 0;  i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+///Stampo il cibo nella prima posizione 
+Console.WriteLine(cibiPreferiti[0]);
+
+///Stampo il cibo nell'ultima posizione 
+Console.WriteLine(cibiPreferiti[cibiPreferiti.Length - 1]);
+
+///ciaonnfw
+
